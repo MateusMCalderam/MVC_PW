6139dd4b20419279b8d1bb02c117e76fce6100b8
Mateus Maschio Calderam <125042084+MateusMCalderam@users.noreply.github.com>
2024-10-03 15:17:08 -0300

    Update CRUD Alunos

--- MVC_PW/controllers/LivrosController.php
+++ MVC_PW/controllers/LivrosController.php
@@ -34,35 +34,35 @@ final class LivrosController extends Controller {
 
     public function save() {
 
-        $id = $_GET["id"] ?? 0;
-        $erro = "";
-        if (
-            empty($_POST["titulo"]) ||
-            empty($_POST["autores"]) ||
-            empty($_POST["editora"]) ||
-            empty($_POST["ano_publicacao"]) ||
-            empty($_POST["quantidade_exemplares"]) ||
-            empty($_POST["isbn"])
-        ) {
-            $erro = "Erro: Todos os campos são obrigatórios!";
-        }
-        $isbnLength = strlen($_POST["isbn"]);
-        if ($isbnLength !== 10 && $isbnLength !== 13) {
-            $erro = "Erro: O ISBN deve ter 10 ou 13 caracteres!";
-        }
-        if (!is_numeric($_POST["quantidade_exemplares"]) || $_POST["quantidade_exemplares"] < 0) {
-            $erro = "Erro: A quantidade de exemplares deve ser um número válido e maior ou igual a 0!";
-        }
-        if (strlen($_POST["ano_publicacao"]) !== 4 || !is_numeric($_POST["ano_publicacao"])) {
-            $erro = "Erro: O ano de publicação deve ter exatamente 4 dígitos!";
-        }
-        echo "aaaaaaaaaaaaaaaaa";
-        if (empty($erro)) {
-            $this->loadView("formLivros", [
-                "erro" => $erro
-            ]);
-            return;
-        }
+        // $id = $_GET["id"] ?? 0;
+        // $erro = "";
+        // if (
+        //     empty($_POST["titulo"]) ||
+        //     empty($_POST["autores"]) ||
+        //     empty($_POST["editora"]) ||
+        //     empty($_POST["ano_publicacao"]) ||
+        //     empty($_POST["quantidade_exemplares"]) ||
+        //     empty($_POST["isbn"])
+        // ) {
+        //     $erro = "Erro: Todos os campos são obrigatórios!";
+        // }
+        // $isbnLength = strlen($_POST["isbn"]);
+        // if ($isbnLength !== 10 && $isbnLength !== 13) {
+        //     $erro = "Erro: O ISBN deve ter 10 ou 13 caracteres!";
+        // }
+        // if (!is_numeric($_POST["quantidade_exemplares"]) || $_POST["quantidade_exemplares"] < 0) {
+        //     $erro = "Erro: A quantidade de exemplares deve ser um número válido e maior ou igual a 0!";
+        // }
+        // if (strlen($_POST["ano_publicacao"]) !== 4 || !is_numeric($_POST["ano_publicacao"])) {
+        //     $erro = "Erro: O ano de publicação deve ter exatamente 4 dígitos!";
+        // }
+        // echo "aaaaaaaaaaaaaaaaa";
+        // if (empty($erro)) {
+        //     $this->loadView("formLivros", [
+        //         "erro" => $erro
+        //     ]);
+        //     return;
+        // }
 
         print_r($_POST);
         $id = $_POST["id"];
