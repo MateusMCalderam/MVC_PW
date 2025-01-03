91bade0092b05551215d773ce7a1dddd2d6d9b86
Mateus Maschio Calderam <mateusmmcalderam@gmail.com>
2025-01-03 00:45:52 -0300

    Remove Pasta Raiz

--- MVC_PW/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("HOST", "localhost");
-define("BASE", "trab3_pw");
-define("USER", "root");
-define("PASS", "");
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("HOST", "localhost");
+define("BASE", "trab3_pw");
+define("USER", "root");
+define("PASS", "");
